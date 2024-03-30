5d3703e3be717554a86c0c27168f97375b36cf56
Urban Versis 32 <107663697+UrbanVersis32@users.noreply.github.com>
2024-03-30 16:22:24 -0500

    Add headers
    Add headers for each command for 1.0.2

--- bin/display.sh
+++ bin/display.sh
@@ -4,16 +4,20 @@ space() {
 	printf "\n\n"
 	}
 
+echo "IP address"
 ip address show
 
 space
 
+echo "netstat"
 netstat -anp
 
 space
 
+echo "lsoft"
 lsof
 
+echo "w and who"
 space
 
 w
@@ -24,10 +28,12 @@ who
 
 space
 
+echo "ps"
 ps -a
 
 space
 
+echo "sestatus"
 sestatus
 
 space
